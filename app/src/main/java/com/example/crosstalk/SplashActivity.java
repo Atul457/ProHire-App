8a4311fff22ac192c0e6858ce9965757f6825ae1
rutu457 <bhenderutu06@gmail.com>
2023-02-19 20:41:13 +0530

    Create login page, and add backend code for navigation from splash screen to login

--- app/src/main/java/com/example/crosstalk/SplashActivity.java
+++ app/src/main/java/com/example/crosstalk/SplashActivity.java
@@ -1,8 +1,11 @@
+
 package com.example.crosstalk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+         Handler h1 = new Handler();
+         h1.postDelayed(new Runnable() {
+             @Override
+             public void run() {
+                 Intent i = new Intent(SplashActivity.this,LoginActivity.class);
+                 startActivity(i);
+                 finish();
+
+             }
+         },3000);
+
     }
 }
