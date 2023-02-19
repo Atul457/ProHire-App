178256a590fd519359fd7a62223d24f68ddc3c88
zubairfaizi <mrzubairfaizi@gmail.com>
2023-02-19 22:53:36 +0530

    Setup

--- app/src/main/java/com/example/crosstalk/SplashActivity.java
+++ app/src/main/java/com/example/crosstalk/SplashActivity.java
@@ -2,13 +2,16 @@ package com.example.crosstalk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
     }
 }
