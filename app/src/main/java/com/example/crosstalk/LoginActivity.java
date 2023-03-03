1c939f7c56c5d794959b3ba1f568fcc80243acf0
rutu457 <bhenderutu06@gmail.com>
2023-03-03 23:12:27 +0530

    Update

--- app/src/main/java/com/example/crosstalk/LoginActivity.java
+++ app/src/main/java/com/example/crosstalk/LoginActivity.java
@@ -1,8 +1,7 @@
 package com.example.crosstalk;
 
+// Imports
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,63 +10,69 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    // Vars
     TextView login_username;
     TextView login_password;
     TextView signupText;
 
     TextView loginButton;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        getSupportActionBar().hide();
+        if(getSupportActionBar() != null) getSupportActionBar().hide();
 
         login_password = findViewById(R.id.login_password);
         login_username = findViewById(R.id.login_username);
         loginButton = findViewById(R.id.loginButton);
 
-
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (login_username.getText().toString().isEmpty())
-                {
-                    login_username.setError(("please enter your user name "));
-                }
-                else if (login_password.getText().toString().isEmpty())
-                {
-                    login_password.setError("please enter your user name ");
-                }
-                else if (login_username.getText().toString().length() <= 8)
-                {
-                    login_username.setError("username must be gerten than 8 ");
-                }
-                else if (login_password.getText().toString().length() <= 8)
-                {
-                    login_password.setError("password must be gerten than 8 ");
-                }
-                else if  (!login_username.getText().toString().contains("@") || !login_username.getText().toString().contains(".com"));
-                {
-                   login_username.setError("Incorrct username");
-                }  else if(true)
-                {
-                    Toast.makeText(LoginActivity.this,"login successfully done",Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
-                    startActivity(intent);
-                    finish();
+
+                // Min and Max length
+                int minMaxLength = 8;
+
+                // Validations
+
+                if (login_username.getText().toString().isEmpty()) {
+                    login_username.setError(("username is required"));
+                    return;
                 }
 
+                if (login_password.getText().toString().isEmpty()) {
+                    login_password.setError("password is required");
+                    return;
+                }
+
+                if (login_username.getText().toString().length() <= minMaxLength) {
+                    login_username.setError("username length should be greater than 8");
+                    return;
+                }
+
+                if (login_password.getText().toString().length() <= minMaxLength) {
+                    login_password.setError("password length must be greater than 8");
+                    return;
+                }
+
+                if (!login_username.getText().toString().contains("@") || !login_username.getText().toString().contains(".com")) {
+                    login_username.setError("Incorrect username");
+                    return;
+                }
+
+                Toast.makeText(LoginActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
 
             }
         });
 
         signupText = findViewById(R.id.signupText);
-        signupText.setOnClickListener(new View.OnClickListener()
-
-        {
+        signupText.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this,RegistrationActivity.class);
